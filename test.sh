d8342ebbcd2923261f480bd1cdb6d0f3743aa793
Mike Forbes <mike@nothing.net.nz>
2017-10-15 22:23:10 +1300

    this is awful

--- test.sh
+++ test.sh
@@ -1,51 +1,12 @@
 #!/bin/bash
 set -e
-set -o pipefail
-# this is kind of an expensive check, so let's not do this twice if we
-# are running more than one validate bundlescript
-VALIDATE_REPO='https://github.com/mikeforbes/dockerfiles.git'
-VALIDATE_BRANCH='master'
+files=`ls */Dockerfile|cut -d"/" -f1`
 
-VALIDATE_HEAD="$(git rev-parse --verify HEAD)"
-
-git fetch -q "$VALIDATE_REPO" "refs/heads/$VALIDATE_BRANCH"
-VALIDATE_UPSTREAM="$(git rev-parse --verify FETCH_HEAD)"
-
-VALIDATE_COMMIT_DIFF="$VALIDATE_UPSTREAM...$VALIDATE_HEAD"
-
-validate_diff() {
-	if [ "$VALIDATE_UPSTREAM" != "$VALIDATE_HEAD" ]; then
-		git diff "$VALIDATE_COMMIT_DIFF" "$@"
-	else
-		git diff HEAD~ "$@"
-	fi
-}
-
-# get the dockerfiles changed
-IFS=$'\n'
-files=( $(validate_diff --name-only -- '*Dockerfile') )
-unset IFS
-# build the changed dockerfiles
-for f in "${files[@]}"; do
-	if ! [[ -e "$f" ]]; then
-		continue
-	fi
-
-	image=${f%Dockerfile}
-	base=${image%%\/*}
-	suite=${image##*\/}
-	build_dir=$(dirname $f)
-
-	if [[ -z "$suite" ]]; then
-		suite=latest
-	fi
-
-	(
-	set -x
-	docker build -t ${base}:${suite} ${build_dir}
-	)
-
+for f in $files; do
+        cd $f
+	docker build -t $f:latest .
+        cd ..
 	echo "                       ---                                   "
-	echo "Successfully built ${base}:${suite} with context ${build_dir}"
+	echo "Successfully built $f"
 	echo "                       ---                                   "
 done
