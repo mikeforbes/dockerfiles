e599cbcc546ce912a09ed402a098ffbeb7ff39d2
Mike Forbes <mike@nothing.net.nz>
2018-03-22 20:15:04 +1300

    build 'em fresh :hamburger:

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@ files=`ls */Dockerfile|cut -d"/" -f1`
 
 for f in $files; do
         cd $f
-	docker build -t $f:latest .
+	docker build --no-cache -t $f:latest .
         cd ..
 	echo "                       ---                                   "
 	echo "Successfully built $f"
